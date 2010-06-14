099c3bb582e4f29204c1372bb4fc31e33caa8375
Vojtech Kusy <wojtha@gmail.com>
2010-06-14 15:19:10 +0200

    Preparing for deploy: settings moved to mediareference.settings.php, added INSTALL and LICENSE files.

---
+++ mediareference.settings.php
@@ -0,0 +1,23 @@
+<?php
+
+//=========================================
+//    CONFIGURATION
+//=========================================
+
+// Image
+define('MEDIAREF_IMAGE_TYPE', 'image');
+define('MEDIAREF_IMAGE_THUMBNAIL', 'field_image');
+define('MEDIAREF_IMAGE_FIELDS', 'title|description|link');
+
+// Video
+define('MEDIAREF_VIDEO_TYPE', 'video');
+define('MEDIAREF_VIDEO_THUMBNAIL', 'field_image');
+define('MEDIAREF_VIDEO_FIELDS', 'title|description');
+
+// Slideshow
+define('MEDIAREF_SLIDESHOW_TYPE', 'slideshow');
+define('MEDIAREF_SLIDESHOW_THUMBNAIL', 'field_image');
+define('MEDIAREF_SLIDESHOW_FIELDS', 'title|description|link|colour');
+
+// Imagecache
+define('MEDIAREF_IMAGECACHE_THUMB_PROFILE', 'mediareference_thumbs');
