12d16ec4ed4a61d4400c29c02eddfaecc4eb506a
Vojtech Kusy <wojtha@gmail.com>
2010-10-30 14:36:49 +0200

    Moving Lightbox2 support/dependency to separate sub-module

--- includes/mediareference.js
+++ includes/mediareference.js
@@ -40,60 +40,7 @@
 })(jQuery);
 
 
-/**
- * Stripped down method Lightbox.start to allow us launch only "Lightframe" and also do it programatically
- */
-function LightframeStart(href, title, alt, style) {
 
-  Lightbox.isPaused = !Lightbox.autoStart;
-
-  // Replaces hideSelectBoxes() and hideFlash() calls in original lightbox2.
-  Lightbox.toggleSelectsFlash('hide');
-
-  Lightbox.isSlideshow = false;
-  Lightbox.isLightframe = true;
-  Lightbox.isVideo = false;
-  Lightbox.isModal = false;
-  Lightbox.imageArray = [];
-  Lightbox.imageNum = 0;
-
-  // Stretch overlay to fill page and fade in.
-  var arrayPageSize = Lightbox.getPageSize();
-  $("#lightbox2-overlay")
-    .hide()
-    .css({
-      'width': '100%',
-      'zIndex': '10090',
-      'height': arrayPageSize[1] + 'px',
-      'backgroundColor' : '#' + Lightbox.overlayColor,
-      'opacity' : Lightbox.overlayOpacity
-    })
-    .removeClass("overlay_macff2")
-    .addClass("overlay_default")
-    .fadeIn(Lightbox.fadeInSpeed);
-
-  Lightbox.imageArray.push([href, title, alt, style]);
-
-  $('#frameContainer, #modalContainer, #lightboxImage').hide();
-  $('#hoverNav, #prevLink, #nextLink, #frameHoverNav, #framePrevLink, #frameNextLink').hide();
-  $('#imageDataContainer, #numberDisplay, #bottomNavZoom, #bottomNavZoomOut').hide();
-  $('#outerImageContainer').css({'width': '250px', 'height': '250px'});
-
-  // Calculate top and left offset for the lightbox.
-  var arrayPageScroll = Lightbox.getPageScroll();
-  var lightboxTop = arrayPageScroll[1] + (Lightbox.topPosition == '' ? (arrayPageSize[3] / 10) : Lightbox.topPosition) * 1;
-  var lightboxLeft = arrayPageScroll[0];
-  $('#lightbox')
-    .css({
-      'zIndex': '10500',
-      'top': lightboxTop + 'px',
-      'left': lightboxLeft + 'px'
-    })
-    .show();
-
-  Lightbox.total = Lightbox.imageArray.length;
-  Lightbox.changeData(Lightbox.imageNum);
-};
 
 /**
  *  Trigger update event on the button which started the lightframe
@@ -133,9 +80,8 @@ Drupal.behaviors.mediareference = function(context) {
         if (ahahSettings) {
           var params = ahahSettings.url.split('/');
           var href = '/mediabrowser/' + params[2] + '/' + params[3];
-          var alt = title = 'Media Browser';
-          var style = 'width:900px; height:500px; scrolling:yes; disableCloseClick:false';
-          LightframeStart(href, title, alt, style);
+          // launch browser in popup
+          Drupal.settings.lightframe.start(href);
           // save this to global variable for later use
           Drupal.settings.lightframe.updateAfterClose = this;
         }
